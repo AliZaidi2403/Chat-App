e833cf489ed529c88e2a4089a22925fa8ab53f84
Amaan <amaanzaidi462@gmail.com>
2024-02-13 00:41:03 +0530

    final commit

--- Backend/server.js
+++ Backend/server.js
@@ -18,20 +18,22 @@ app.use((req, res, next) => {
   next();
 });
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
-app.use(notFound);
-app.use(errorHandler);
+
 //-----------------Deployement-----------------------
 
 const __dirname1 = path.resolve();
 app.use(express.static(path.join(__dirname1, "/frontend/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+});
 //--------------------------------------------------------------
+
+app.use(notFound);
+app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port, console.log(`Server runninng at port ${port}`));
